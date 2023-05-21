29e30b0a8ec1f099cbf7ecf6bc16da7896a1d33b
kunal11295 <kunalborkar3325@gmail.com>
2023-05-21 10:52:41 +0530

    update

---
+++ login.js
@@ -0,0 +1,26 @@
+function login(event) {
+  event.preventDefault();
+
+  var userEmail = document.getElementById("useremail").value;
+  var userPassword = document.getElementById("userpassword").value;
+  var loginuser = {};
+
+  var Ls = JSON.parse(localStorage.getItem("Instagramusers"));
+
+  var flag = false;
+  for (var i = 0; i < Ls.length; i++) {
+    if (Ls[i].userEmail == userEmail && Ls[i].userPassword == userPassword) {
+      flag = true;
+      loginuser = Ls[i];
+    }
+  }
+  if (flag == true) {
+    localStorage.setItem("InstagramCurrentUser", JSON.stringify(loginuser));
+
+    alert("login successfull");
+    window.location.href = `./home.html`;
+  } 
+  // else {
+  //   alert("credintials not matched");
+  // }
+}
